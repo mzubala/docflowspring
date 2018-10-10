b358771831341c1f66ed689e3cc20d13b0398f93
Maciej Zubala <maciej.zubala@allegro.pl>
2018-10-10 22:39:57 +0200

    missing command

---
+++ docflow/src/main/java/pl/com/bottega/docflowjee/docflow/commands/PassToVerificationCommand.java
@@ -0,0 +1,8 @@
+package pl.com.bottega.docflowjee.docflow.commands;
+
+import java.util.UUID;
+
+public class PassToVerificationCommand {
+    public UUID documentId;
+    public Long employeeId;
+}
