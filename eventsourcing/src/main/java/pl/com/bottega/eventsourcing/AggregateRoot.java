8b42f340fc7c390a4e632bc35cb4fbf1d291f52b
Maciej Zubala <maciej.zubala@allegro.pl>
2018-10-12 21:31:52 +0200

    dispatch

--- eventsourcing/src/main/java/pl/com/bottega/eventsourcing/AggregateRoot.java
+++ eventsourcing/src/main/java/pl/com/bottega/eventsourcing/AggregateRoot.java
@@ -1,5 +1,9 @@
 package pl.com.bottega.eventsourcing;
 
+import io.vavr.control.Try;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -33,7 +37,7 @@ public abstract class AggregateRoot {
 
     protected void applyChange(Event event, boolean isNew) {
         dispatch(event);
-        if(isNew) {
+        if (isNew) {
             changes.add(event);
         }
     }
@@ -42,6 +46,22 @@ public abstract class AggregateRoot {
         throw new IllegalArgumentException(String.format("Event {} is not supported", event));
     }
 
-    protected abstract void dispatch(Event event);
+    protected void dispatch(Event event) {
+        try {
+            Method applyMethod = getClass().getDeclaredMethod("apply", event.getClass());
+            applyMethod.setAccessible(true);
+            applyMethod.invoke(this, event);
+            applyMethod.setAccessible(false);
+        } catch(InvocationTargetException | IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        } catch (NoSuchMethodException ex) {
+            throw noApplyMethodError(event, ex);
+        }
+    }
+
+    private IllegalArgumentException noApplyMethodError(Event event, NoSuchMethodException e) {
+        return new IllegalArgumentException(String.format("No method void apply(%s event) found in the aggregate %s",
+            event.getClass().getName(), getClass().getName()), e);
+    }
 
 }
