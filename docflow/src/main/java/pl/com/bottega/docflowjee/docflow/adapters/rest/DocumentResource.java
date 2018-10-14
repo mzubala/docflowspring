297d04b4ed6bb6b4236bb908041303fd43849f54
Maciej Zubala <maciej.zubala@allegro.pl>
2018-10-14 21:53:00 +0200

    prep

--- docflow/src/main/java/pl/com/bottega/docflowjee/docflow/adapters/rest/DocumentResource.java
+++ docflow/src/main/java/pl/com/bottega/docflowjee/docflow/adapters/rest/DocumentResource.java
@@ -16,11 +16,13 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/documents")
 @Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class DocumentResource {
 
     @Inject
