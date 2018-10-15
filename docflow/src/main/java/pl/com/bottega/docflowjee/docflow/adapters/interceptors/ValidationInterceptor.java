2c59bfc3740b6b3f5a7792093aa9b023ed891c2b
Maciej Zubala <maciej.zubala@allegro.pl>
2018-10-15 21:53:13 +0200

    removed solution

--- docflow/src/main/java/pl/com/bottega/docflowjee/docflow/adapters/interceptors/ValidationInterceptor.java
+++ docflow/src/main/java/pl/com/bottega/docflowjee/docflow/adapters/interceptors/ValidationInterceptor.java
@@ -21,24 +21,7 @@ public class ValidationInterceptor {
 
     @AroundInvoke
     public Object intercept(InvocationContext ctx) throws Exception {
-        checkUsage(ctx);
-        Command command = (Command) ctx.getParameters()[0];
-        if(command == null) {
-            throw new IllegalArgumentException("Null command passed to service");
-        }
-        Set<ConstraintViolation<Command>> errors = validator.validate(command);
-        if(!errors.isEmpty()) {
-            throw new ConstraintViolationException("Command is invalid", errors);
-        }
         return ctx.proceed();
-    }
-
-    private void checkUsage(InvocationContext ctx) {
-        Class<?>[] types = ctx.getMethod().getParameterTypes();
-        if(types.length != 1 || !Command.class.isAssignableFrom(types[0])) {
-            throw new IllegalArgumentException("@ValidateCommand annotation can only be used on methods " +
-                "that take command as single argument.");
-        }
     }
 
 }
--- docflow/src/main/java/pl/com/bottega/docflowjee/docflow/commands/CreateDocumentCommand.java
+++ docflow/src/main/java/pl/com/bottega/docflowjee/docflow/commands/CreateDocumentCommand.java
@@ -2,14 +2,11 @@ package pl.com.bottega.docflowjee.docflow.commands;
 
 import pl.com.bottega.eventsourcing.Command;
 
-import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 public class CreateDocumentCommand implements Command {
 
-    @NotNull
     public UUID documentId;
-    @NotNull
     public Long employeeId;
 
     public CreateDocumentCommand(UUID documentId, Long employeeId) {
