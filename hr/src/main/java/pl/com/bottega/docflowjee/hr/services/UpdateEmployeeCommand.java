38721a99a4f0f8527bc84d5fc9d3dcd8056bf063
Maciej Zubala <maciej.zubala@gmail.com>
2020-09-12 22:26:47 +0200

    task 1 prep

---
+++ hr/src/main/java/pl/com/bottega/docflowjee/hr/services/UpdateEmployeeCommand.java
@@ -0,0 +1,31 @@
+package pl.com.bottega.docflowjee.hr.services;
+
+import lombok.Getter;
+
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+import static org.apache.logging.log4j.util.Strings.isNotBlank;
+
+@Getter
+public class UpdateEmployeeCommand {
+	private final Long id;
+	private final String firstName;
+	private final String lastName;
+	private final List<Long> departmentIds;
+	private final Long supervisorId;
+
+	public UpdateEmployeeCommand(Long id, String firstName, String lastName, List<Long> departmentIds, Long supervisorId) {
+		checkNotNull(id);
+		checkState(isNotBlank(firstName));
+		checkState(isNotBlank(lastName));
+		checkNotNull(departmentIds);
+		checkState(departmentIds.size() > 0);
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.departmentIds = departmentIds;
+		this.supervisorId = supervisorId;
+	}
+}
