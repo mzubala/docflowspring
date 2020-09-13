6552f4ec5992e94919bc5ad93984f9ba6a99724a
Maciej Zubala <maciej.zubala@gmail.com>
2020-09-13 20:15:13 +0200

    prep

--- docflow/src/main/java/pl/com/bottega/docflowjee/docflow/adapters/rest/ExceptionMappers.java
+++ docflow/src/main/java/pl/com/bottega/docflowjee/docflow/adapters/rest/ExceptionMappers.java
@@ -3,7 +3,7 @@ package pl.com.bottega.docflowjee.docflow.adapters.rest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
-public class ExceptionHandlers {
+public class ExceptionMappers {
 
 
 }
