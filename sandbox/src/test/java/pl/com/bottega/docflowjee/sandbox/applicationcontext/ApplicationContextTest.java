a372c63dc7741001fad34d86daa8d35e53809859
Maciej Zubala <maciej.zubala@gmail.com>
2021-06-10 08:21:07 +0200

    sandbox demo

--- sandbox/src/test/java/pl/com/bottega/docflowjee/sandbox/applicationcontext/ApplicationContextTest.java
+++ sandbox/src/test/java/pl/com/bottega/docflowjee/sandbox/applicationcontext/ApplicationContextTest.java
@@ -1,18 +1,23 @@
 package pl.com.bottega.docflowjee.sandbox.applicationcontext;
 
 import org.junit.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class ApplicationContextTest {
 
     @Test
-    public void createsApplicationContext() {
+    public void createsApplicationContextAndGetsBeans() {
         ApplicationContext context = new AnnotationConfigApplicationContext(getClass().getPackage().getName());
 
+        assertThat(context.getBean("exampleComponent")).isNotNull();
+        assertThat(context.getBean("foo")).isNotNull();
         assertThat(context.getBean(ExampleComponent.class)).isNotNull();
+        assertThatThrownBy(() -> context.getBean(ExampleInterface.class)).isInstanceOf(NoUniqueBeanDefinitionException.class);
     }
 
 }
